14bde8e8f6eeaad7d59948d56e73093ad851cd75
NoobCoder143 <avinav.chowdhury7@gmail.com>
2025-06-26 01:02:45 +0530

    added logo

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,13 +3,18 @@ import Navbar from '@components/Navbar';
 import Provider from '@components/Provider';
 
 export const metadata = {
-  title: "PromptShare ",
+  title: "PromptShare",
   description: "Share and explore various useful prompts"
 };
 
 export default function RootLayout({ children ,session}) {
   return (
     <html lang="en">
+       <head>
+        <link rel="icon" href="/assets/icons/artificial-intelligence.png" />
+        <title>PromptShare</title>
+        <meta name="description" content="Share and explore various useful prompts" />
+      </head>
       <body>
         <Provider session={session}>
           <div className="main">
